d2d07088967e1371334258123322cd4dd7ffe31b
ModyHany <48627170+ModyHany@users.noreply.github.com>
2019-04-06 21:19:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2175,4 +2175,4 @@ if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return mess
 
 
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.NDUxODM3ODUwMDUzMzc4MDY5.XKjshg.q6o8snX1YvGutcdzGwtz7uweLQI);
